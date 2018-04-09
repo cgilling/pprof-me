965794cebc17d64c3dade5586b139ed6c5f77155
Chris Gilling <cgilling@iparadigms.com>
2018-04-08 22:09:21 -0700

    add client pkg to help with easy interaction
    * implemented SendProfile
    * tests are using a yet to be published httpmock lib by dkinder,
      so things won't work outside of my local machine until he does

---
+++ client/client.go
@@ -0,0 +1,117 @@
+package client
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"github.com/cgilling/pprof-me/msg"
+	"github.com/dghubble/sling"
+)
+
+type Doer interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type Client struct {
+	base      *sling.Sling
+	urlPrefix string
+	binaryMD5 string
+
+	SymbolURL string
+}
+
+/*
+type Config struct {
+	Doer       Doer
+	URLPrefix  string
+	SymbolPath string
+}
+*/
+
+func New(urlPrefix string, d Doer) (*Client, error) {
+	if d == nil {
+		d = http.DefaultClient
+	}
+	if urlPrefix[len(urlPrefix)-1] != '/' {
+		urlPrefix += "/"
+	}
+	fp, err := os.Open(os.Args[0])
+	if err != nil {
+		return nil, fmt.Errorf("failed to open binary to create md5 hash: %v", err)
+	}
+	defer fp.Close()
+	h := md5.New()
+	_, err = io.Copy(h, fp)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create md5 of binary: %v", err)
+	}
+	return &Client{
+		base:      sling.New().Base(urlPrefix).Doer(d),
+		urlPrefix: urlPrefix,
+		binaryMD5: hex.EncodeToString(h.Sum(nil)),
+	}, nil
+}
+
+func (c *Client) SendProfile(ctx context.Context, name string, r io.Reader) (string, error) {
+	profile, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	preq := msg.ProfilePostRequest{
+		Profile:    profile,
+		BinaryName: filepath.Base(os.Args[0]),
+		BinaryMD5:  c.binaryMD5,
+	}
+	if c.SymbolURL != "" {
+		preq.SymoblizerURL = c.SymbolURL
+	}
+
+	var presp msg.ProfilePostResponse
+	resp, err := c.base.New().Post("profiles").BodyJSON(preq).ReceiveSuccess(&presp)
+	if err != nil {
+		return "", err
+	}
+	if exp, got := 201, resp.StatusCode; exp != got {
+		return "", fmt.Errorf("POST /profiles returned unexpected status code: exp: %d, got: %d", exp, got)
+	}
+
+	if presp.BinaryNeedsUpload {
+		var s string
+		fp, err := os.Open(os.Args[0])
+		if err != nil {
+			return presp.ID, err
+		}
+		defer fp.Close()
+		resp, err = c.base.New().Put("binaries/").Path(c.binaryMD5).Body(fp).ReceiveSuccess(&s)
+		if err != nil {
+			return presp.ID, err
+		}
+		if exp, got := 204, resp.StatusCode; exp != got {
+			return presp.ID, fmt.Errorf("POST /binaries/:id returned unexpected status code: exp: %d, got: %d", exp, got)
+		}
+	}
+	return presp.ID, nil
+}
+
+func getLocalAddr(ctx context.Context, targetURL string) (string, error) {
+	d := &net.Dialer{}
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return "", err
+	}
+	conn, err := d.DialContext(ctx, "tcp", u.Host)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+	return conn.LocalAddr().String(), nil
+}
---
+++ client/client_test.go
@@ -0,0 +1,194 @@
+package client
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/cgilling/pprof-me/httpmock"
+	"github.com/cgilling/pprof-me/msg"
+	"github.com/stretchr/testify/mock"
+)
+
+var binaryMD5 string
+
+func init() {
+	// NOTE: I normally dislike implementing logic in tests that directly
+	//		 replicates how the code itself calculates the same value, but
+	//		 in this instance there doesn't seem to be a better way of doing
+	//       this as the md5 of the test binary will be changed with every
+	//       change to the test (or more), so this value can't be merely
+	//       hardcoded.
+	fp, err := os.Open(os.Args[0])
+	if err != nil {
+		panic(fmt.Errorf("failed to open binary to create md5: %v", err))
+	}
+	defer fp.Close()
+	h := md5.New()
+	_, err = io.Copy(h, fp)
+	if err != nil {
+		panic(fmt.Errorf("failed to create md5 of binary: %v", err))
+	}
+	binaryMD5 = hex.EncodeToString(h.Sum(nil))
+}
+
+func TestClientWithSymbols(t *testing.T) {
+	ppme := &httpmock.MockHandler{}
+
+	s := httpmock.NewServer(ppme)
+	defer s.Close()
+
+	c, err := New(s.URL(), nil)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	c.SymbolURL = "http://me/custom/symbols"
+
+	msgResp := msg.ProfilePostResponse{
+		ID:                "23E3490F-9F1F-4A19-9EBB-07592A7A1ED0",
+		BinaryNeedsUpload: false,
+	}
+	msgReq := msg.ProfilePostRequest{
+		Profile:       []byte("hellothere"),
+		BinaryName:    filepath.Base(os.Args[0]),
+		BinaryMD5:     binaryMD5,
+		SymoblizerURL: c.SymbolURL,
+	}
+
+	ppme.On("Handle", "POST", "/profiles", httpmock.JSONMatcher(&msgReq)).Return(httpmock.Response{
+		Status: 201,
+		Body:   httpmock.ToJSON(msgResp),
+	})
+
+	id, err := c.SendProfile(context.Background(), "TestClientWithSymbols", strings.NewReader("hellothere"))
+	if err != nil {
+		t.Fatalf("failed to SendProfile: %v", err)
+	}
+	if exp, got := msgResp.ID, id; exp != got {
+		t.Errorf("returned ID not as expected: exp: %v, got: %v", exp, got)
+	}
+
+	ppme.AssertExpectations(t)
+}
+
+func TestClientUploadsBinary(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	ppme := &httpmock.MockHandler{}
+
+	s := httpmock.NewServer(ppme)
+	defer s.Close()
+
+	c, err := New(s.URL(), nil)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	msgResp := msg.ProfilePostResponse{
+		ID:                "23E3490F-9F1F-4A19-9EBB-07592A7A1ED0",
+		BinaryNeedsUpload: true,
+	}
+	msgReq := msg.ProfilePostRequest{
+		Profile:    []byte("hellothere"),
+		BinaryName: filepath.Base(os.Args[0]),
+		BinaryMD5:  binaryMD5,
+	}
+
+	ppme.On("Handle", "POST", "/profiles", httpmock.JSONMatcher(&msgReq)).Return(httpmock.Response{
+		Status: 201,
+		Body:   httpmock.ToJSON(msgResp),
+	})
+
+	bin, err := ioutil.ReadFile(os.Args[0])
+	if err != nil {
+		t.Fatalf("failed to read binary: %v", err)
+	}
+
+	binMatch := mock.MatchedBy(func(arg []byte) bool { return reflect.DeepEqual(arg, bin) })
+	ppme.On("Handle", "PUT", fmt.Sprintf("/binaries/%s", binaryMD5), binMatch).Return(httpmock.Response{
+		Status: 204,
+	})
+
+	id, err := c.SendProfile(context.Background(), "TestClientUploadsBinary", strings.NewReader("hellothere"))
+	if err != nil {
+		t.Fatalf("failed to SendProfile: %v", err)
+	}
+	if exp, got := msgResp.ID, id; exp != got {
+		t.Errorf("returned ID not as expected: exp: %v, got: %v", exp, got)
+	}
+
+	ppme.AssertExpectations(t)
+}
+
+func TestSendProfileReturnsErrorOnNon201Response(t *testing.T) {
+	ppme := &httpmock.MockHandler{}
+
+	s := httpmock.NewServer(ppme)
+	defer s.Close()
+
+	c, err := New(s.URL(), nil)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	ppme.On("Handle", "POST", "/profiles", mock.Anything).Return(httpmock.Response{
+		Status: 500,
+	})
+
+	_, err = c.SendProfile(context.Background(), "TestSendProfileReturnsErrorOnNon201Response", strings.NewReader("hellothere"))
+	if err == nil {
+		t.Errorf("expected SendProfile to return error but it didn't")
+	}
+
+	ppme.AssertExpectations(t)
+}
+
+func TestSendProfileReturnsErrorOnNon204OnUploadBinary(t *testing.T) {
+	ppme := &httpmock.MockHandler{}
+
+	s := httpmock.NewServer(ppme)
+	defer s.Close()
+
+	c, err := New(s.URL(), nil)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	msgResp := msg.ProfilePostResponse{
+		ID:                "23E3490F-9F1F-4A19-9EBB-07592A7A1ED0",
+		BinaryNeedsUpload: true,
+	}
+	msgReq := msg.ProfilePostRequest{
+		Profile:    []byte("hellothere"),
+		BinaryName: filepath.Base(os.Args[0]),
+		BinaryMD5:  binaryMD5,
+	}
+
+	ppme.On("Handle", "POST", "/profiles", httpmock.JSONMatcher(&msgReq)).Return(httpmock.Response{
+		Status: 201,
+		Body:   httpmock.ToJSON(msgResp),
+	})
+
+	ppme.On("Handle", "PUT", fmt.Sprintf("/binaries/%s", binaryMD5), mock.Anything).Return(httpmock.Response{
+		Status: 500,
+	})
+
+	id, err := c.SendProfile(context.Background(), "TestSendProfileReturnsErrorOnNon204OnUploadBinary", strings.NewReader("hellothere"))
+	if err == nil {
+		t.Error("expected SendProfile to return error but it didn't")
+	}
+	if exp, got := msgResp.ID, id; exp != got {
+		t.Errorf("returned ID not as expected: exp: %v, got: %v", exp, got)
+	}
+
+	ppme.AssertExpectations(t)
+}
